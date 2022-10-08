307f0f5dcd4dbbc16362da5cb2652689745f80dc
Ghedeon Nts <Contact.ntsiassila@gmail.com>
2022-10-08 17:57:18 +0200

    configuration de base du server et ajout et configuration de jasmine(framework de test)

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,20 @@
 "use strict";
-console.log('typescript is running');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const PORT = 3000;
+//create instance server
+const app = (0, express_1.default)();
+// add routing for / path
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Hello world 🌍',
+    });
+});
+// start server express
+app.listen(PORT, () => {
+    console.log(`My server running at port : ${PORT}`);
+});
+exports.default = app;
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log('hello world');
+import express, { Application, Request, Response } from 'express';
+
+const PORT = 3000;
+
+//create instance server
+const app: Application = express();
+
+// add routing for / path
+app.get('/', (req: Request, res: Response) => {
+  res.json({
+    message: 'Hello world 🌍',
+  });
+});
+
+// start server express
+app.listen(PORT, () => {
+  console.log(`My server running at port : ${PORT}`);
+});
+
+export default app;
