3249d7e1221ecf6d973d4cfb921e88df13d98a4c
Ghedeon Nts <Contact.ntsiassila@gmail.com>
2022-10-08 21:48:07 +0200

    Ajout de 2 features(middleware express-rate-limit de requête & helmet pour la securite du proto. HTTP

--- dist/index.js
+++ dist/index.js
@@ -3,14 +3,41 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+/**
+ * Import des modules nécessaires
+ */
 const express_1 = __importDefault(require("express"));
+const morgan_1 = __importDefault(require("morgan"));
+const helmet_1 = __importDefault(require("helmet"));
+const express_rate_limit_1 = __importDefault(require("express-rate-limit"));
 const PORT = 3000;
 //create instance server
 const app = (0, express_1.default)();
+// middleware to parse incoming requests
+app.use(express_1.default.json());
+// HTTP request logger middleware
+app.use((0, morgan_1.default)('common'));
+// HTTP security middleware
+app.use((0, helmet_1.default)());
+// Apply the rate limiting middleware to all requests
+app.use((0, express_rate_limit_1.default)({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+    standardHeaders: true,
+    legacyHeaders: false,
+    message: 'Too many accounts created from this IP, please try again after an hour',
+}));
 // add routing for / path
 app.get('/', (req, res) => {
     res.json({
         message: 'Hello world 🌍',
+    });
+});
+// Request Post
+app.use('/', (req, res) => {
+    res.json({
+        message: 'Hello world 🌍 from post',
+        data: req.body,
     });
 });
 // start server express
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,48 @@
+/**
+ * Import des modules nécessaires
+ */
 import express, { Application, Request, Response } from 'express';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import RateLimit from 'express-rate-limit';
 
 const PORT = 3000;
 
 //create instance server
 const app: Application = express();
+// middleware to parse incoming requests
+app.use(express.json());
+
+// HTTP request logger middleware
+app.use(morgan('common'));
+// HTTP security middleware
+app.use(helmet());
+
+// Apply the rate limiting middleware to all requests
+app.use(
+  RateLimit({
+    windowMs: 60 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+    message:
+      'Too many accounts created from this IP, please try again after an hour',
+  })
+);
 
 // add routing for / path
 app.get('/', (req: Request, res: Response) => {
   res.json({
     message: 'Hello world 🌍',
+  });
+});
+
+// Request Post
+app.post('/', (req: Request, res: Response) => {
+  console.log(req.body);
+  res.json({
+    message: 'Hello world 🌍 from post',
+    data: req.body,
   });
 });
 
